1f6a5fb9243678fe1788c0a5232f8583a21667bd
Mustafa Koser <mustufakoser52@gmail.com>
2025-03-28 21:42:53 +0530

    completed desktop view

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
 
-    const menuBtn = document.querySelector('.menu-btn');
-    const navLinks = document.querySelector('.nav-links');
+    // const menuBtn = document.querySelector('.menu-btn');
+    // const navLinks = document.querySelector('.nav-links');
 
-    menuBtn.addEventListener('click', () => {
-        navLinks.style.display = navLinks.style.display === 'flex' ? 'none' : 'flex';
-    });
-
-    // Smooth scroll for navigation links
-    // document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    //     anchor.addEventListener('click', function (e) {
-    //         e.preventDefault();
-    //         document.querySelector(this.getAttribute('href')).scrollIntoView({
-    //             behavior: 'smooth'
-    //         });
-    //     });
+    // menuBtn.addEventListener('click', () => {
+    //     navLinks.style.display = navLinks.style.display === 'flex' ? 'none' : 'flex';
     // });
+
+    // // Smooth scroll for navigation links
+    // // document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    // //     anchor.addEventListener('click', function (e) {
+    // //         e.preventDefault();
+    // //         document.querySelector(this.getAttribute('href')).scrollIntoView({
+    // //             behavior: 'smooth'
+    // //         });
+    // //     });
+    // // });
+
+// const apple = document.querySelector("#apple");
+
+// apple.addEventListener("mouseover", function(event) {
+//     let e = event.target;
+//        e.innerHTML = "<img src=\"assets/Apple-logo.png\" alt=\"Apple Icon\">"; 
+// });
+
+// apple.addEventListener("mouseout", function() {
+//     apple.innerHTML = "<img src=\"assets/white-apple-logo.png\" alt=\"Apple Icon\">";
+// });
